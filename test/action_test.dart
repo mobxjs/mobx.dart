97cdade07f34edcaf80eeeb459ea3b9d0fc075af
pavanpodila <pavan.podila@gmail.com>
2018-12-27 13:22:16 +0530

    action api and tests

--- test/action_test.dart
+++ test/action_test.dart
@@ -67,4 +67,30 @@ main() {
 
     dispose();
   });
+
+  test('action inside autorun should be untracked', () {
+    var x = observable(10);
+    var y = observable(20);
+
+    var total = 0;
+    var a = action(() {
+      return y.value;
+    });
+
+    var d = autorun(() {
+      total = x.value + a();
+    });
+
+    expect(total, equals(30));
+
+    // This should not trigger the autorun as y.value is accessed inside the action(),
+    // which by design should be untracked
+    y.value = 30;
+
+    expect(total, equals(30)); // total should still be 10 + 20
+    x.value = 11;
+    expect(total, equals(41)); // total should still be 11 + 30
+
+    d();
+  });
 }
