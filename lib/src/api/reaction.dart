b1961ca3329334d39880e59ac086906483796c42
pavanpodila <pavan.podila@gmail.com>
2018-12-29 00:27:29 +0530

    reaction and tests

--- lib/src/api/reaction.dart
+++ lib/src/api/reaction.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:mobx/src/api/action.dart';
 import 'package:mobx/src/core/reaction.dart';
 import 'package:mobx/src/utils.dart';
 
@@ -45,6 +46,55 @@ ReactionDisposer autorun(Function fn, {String name, int delay}) {
       }
     }, name: name);
   }
+
+  rxn.schedule();
+  return rxn.dispose;
+}
+
+ReactionDisposer reaction<T>(T Function() predicate, Function(T) effect,
+    {String name, int delay, bool fireImmediately}) {
+  Reaction rxn;
+
+  var effectAction = action((T value) => effect(value), name: name);
+
+  var runSync = (delay == null);
+  var scheduler = delay != null ? createDelayedScheduler(delay) : null;
+
+  var firstTime = true;
+  var isScheduled = false;
+  T value;
+
+  reactionRunner() {
+    isScheduled = false;
+    if (rxn.isDisposed) {
+      return;
+    }
+
+    var changed = false;
+    rxn.track(() {
+      var nextValue = predicate();
+      changed = firstTime || (nextValue != value);
+      value = nextValue;
+    });
+
+    var canInvokeEffect =
+        (firstTime && fireImmediately == true) || (!firstTime && changed);
+    if (canInvokeEffect) {
+      effectAction([value]);
+    }
+    if (firstTime) {
+      firstTime = false;
+    }
+  }
+
+  rxn = Reaction(() {
+    if (firstTime || runSync) {
+      reactionRunner();
+    } else if (!isScheduled) {
+      isScheduled = true;
+      scheduler(reactionRunner);
+    }
+  }, name: name);
 
   rxn.schedule();
   return rxn.dispose;
--- test/reaction_test.dart
+++ test/reaction_test.dart
@@ -1,1 +1,104 @@
-void main() {}
+import 'package:fake_async/fake_async.dart';
+import 'package:mobx/src/api/observable.dart';
+import 'package:mobx/src/api/reaction.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Reaction basics', () {
+    var executed = false;
+    var x = observable(10);
+    var d = reaction(() {
+      return x.value > 10;
+    }, (isGreaterThan10) {
+      executed = true;
+    }, name: 'Basic Reaction');
+
+    expect(executed, isFalse);
+
+    x.value = 11; // false -> true, should invoke effect
+    expect(executed, isTrue);
+    executed = false;
+
+    x.value = 9; // true -> false, should invoke effect
+    expect(executed, isTrue);
+
+    executed = false;
+    d();
+
+    x.value = 11;
+    expect(executed, isFalse); // reaction has been disposed, so no more effects
+  });
+
+  test('Reaction with delay', () {
+    var x = observable(10);
+    var executed = false;
+
+    var d = reaction(() {
+      return x.value > 10;
+    }, (isGreaterThan10) {
+      executed = true;
+    }, delay: 1000);
+
+    fakeAsync((async) {
+      x.value = 11;
+
+      // Even though predicate has changed, effect should not be executed
+      expect(executed, isFalse);
+      async.elapse(Duration(milliseconds: 500));
+      expect(executed, isFalse); // should still be false as 1s has not elapsed
+
+      async.elapse(Duration(milliseconds: 500)); // should now trigger effect
+      expect(executed, isTrue);
+    });
+    d();
+  });
+
+  test('Reaction that fires immediately', () {
+    var x = observable(10);
+    var executed = false;
+
+    var d = reaction(() {
+      return x.value > 10;
+    }, (isGreaterThan10) {
+      executed = true;
+    }, fireImmediately: true);
+
+    expect(executed, isTrue); // should fire immediately
+    d();
+  });
+
+  test('Reaction that fires immediately with delay', () {
+    var x = observable(10);
+    var executed = false;
+
+    var d = reaction(() {
+      return x.value > 10;
+    }, (isGreaterThan10) {
+      executed = true;
+    }, delay: 1000, fireImmediately: true);
+
+    fakeAsync((async) {
+      // Effect should be executed, as we are forcing an immediate change even though there is a delay
+      expect(executed, isTrue);
+
+      x.value = 11; // predicate goes from false -> true
+
+      executed = false;
+      async.elapse(Duration(milliseconds: 500));
+      expect(executed, isFalse); // should still be false as 1s has not elapsed
+
+      async.elapse(Duration(milliseconds: 500)); // should now trigger effect
+      expect(executed, isTrue);
+
+      executed = false;
+      // predicate goes from true -> false, but effect should not run for next 1s
+      x.value = 9;
+
+      expect(executed, isFalse);
+      async.elapse(Duration(milliseconds: 1000)); // should now trigger effect
+      expect(executed, isTrue);
+    });
+
+    d();
+  });
+}
