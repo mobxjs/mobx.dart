c85e454dd18d8c3da62645204a82ea073398c9d4
Joni Katajamäki <joni.katajamaki@gmail.com>
2019-01-06 10:12:51 +0200

    Test that action uses provided context

--- test/action_test.dart
+++ test/action_test.dart
@@ -1,7 +1,11 @@
 import 'package:mobx/src/api/action.dart';
 import 'package:mobx/src/api/observable.dart';
 import 'package:mobx/src/api/reaction.dart';
+import 'package:mobx/src/core/action.dart';
+import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
+
+import 'shared_mocks.dart';
 
 void main() {
   test('Action basics work', () {
@@ -179,5 +183,18 @@ void main() {
     expect(total, equals(300));
 
     d();
+  });
+
+  test('action uses provided context', () {
+    final context = MockContext();
+    void fn() {}
+    final action = Action(context, fn);
+    action();
+
+    verify(context.nameFor('Action'));
+    verify(context.untrackedStart());
+    verify(context.startBatch());
+    verify(context.endBatch());
+    verify(context.untrackedEnd(null));
   });
 }
