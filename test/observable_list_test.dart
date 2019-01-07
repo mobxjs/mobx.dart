f04d6fef691bcfceb1cb63b211c314fdc562e942
pavanpodila <pavan.podila@gmail.com>
2019-01-07 08:49:57 +0530

    more tests

--- test/observable_list_test.dart
+++ test/observable_list_test.dart
@@ -19,25 +19,7 @@ void main() {
       d();
     });
 
-    group('can reportObserved() for read-methods', () {
-      void templateTest(
-          String description, void Function(ObservableList<int>) fn) {
-        test(description, () {
-          final list = ObservableList<int>();
-
-          var count = -1;
-
-          final d = autorun((_) {
-            fn(list);
-            count++;
-          });
-
-          list.add(observable(20));
-          expect(count, equals(1));
-          d();
-        });
-      }
-
+    group('fires reportObserved() for read-methods', () {
       <String, void Function(ObservableList<int>)>{
         'map': (_) => _.map((x) => x),
         'isEmpty': (_) => _.isEmpty,
@@ -46,6 +28,7 @@ void main() {
         'reversed': (_) => _.reversed,
         'single': (_) => _ignoreException(() => _.single),
         'first': (_) => _ignoreException(() => _.first),
+        'last': (_) => _ignoreException(() => _.last),
         'toSet': (_) => _.toSet(),
         'toList': (_) => _.toList(),
         'cast': (_) => _.cast(),
@@ -78,8 +61,29 @@ void main() {
             _.firstWhere((_) => true, orElse: () => observable(0)),
         'every': (_) => _.every((_) => true),
         'any': (_) => _.any((_) => true),
-      }.forEach(templateTest);
+      }.forEach(_templateReadTest);
     });
+  });
+
+  group('fires reportChanged() for write-methods', () {
+    <String, void Function(ObservableList<int>)>{
+      'insertAll': (_) => _.insertAll(0, [observable(100)]),
+      'insert': (_) => _.insert(0, observable(100)),
+      'sort': (_) => _.sort(),
+      'setRange': (_) => _.setRange(0, 0, [observable(100)]),
+      'fillRange': (_) => _.fillRange(0, 0, observable(100)),
+      'replaceRange': (_) => _.replaceRange(0, 0, [observable(100)]),
+      'setAll': (_) => _.setAll(0, [observable(100)]),
+      '[]=': (_) => _[0] = observable(100),
+      'add': (_) => _.add(observable(100)),
+      'addAll': (_) => _.addAll([observable(100)]),
+      'clear': (_) => _.clear(),
+      'removeLast': (_) => _.removeLast(),
+      'remove': (_) => _.remove(null),
+      'removeRange': (_) => _.removeRange(0, 0),
+      'removeAt': (_) => _.removeAt(0),
+      'removeWhere': (_) => _.removeWhere((_) => true),
+    }.forEach(_templateWriteTest);
   });
 }
 
@@ -90,4 +94,44 @@ dynamic _ignoreException(Function fn) {
     // Catching on Object since it takes care of both Error and Exception
     // Ignore
   }
+}
+
+void _templateReadTest(
+    String description, void Function(ObservableList<int>) fn) {
+  test(description, () {
+    final list = ObservableList<int>();
+
+    var count = -1;
+
+    final d = autorun((_) {
+      fn(list); // fire the read-method, causing reportObserved() to be invoked
+      count++;
+    });
+
+    list.add(observable(20));
+    expect(count, equals(1));
+    d();
+  });
+}
+
+void _templateWriteTest(
+    String description, void Function(ObservableList<int>) fn) {
+  test(description, () {
+    final list = ObservableList<int>();
+    list.add(observable(20));
+
+    var count = -1;
+
+    final d = autorun((_) {
+      list.length;
+      count++;
+    });
+
+    // fire the write method, causing reportChanged() to be invoked.
+    // This should be picked up in the autorun()
+    fn(list);
+
+    expect(count, equals(1));
+    d();
+  });
 }
