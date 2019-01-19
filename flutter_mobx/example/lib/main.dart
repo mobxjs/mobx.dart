d0d92908ba529f5be90bb2e3059f690c9c658c37
pavanpodila <pavan.podila@gmail.com>
2019-01-19 18:31:13 +0530

    moving under /example

--- flutter_mobx/example/lib/main.dart
+++ flutter_mobx/example/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:mobx/mobx.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
 void main() => runApp(MyApp());
 
