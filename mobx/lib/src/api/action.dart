a1bddea1084b0b2a6d445a0ce682256c6f5ab29a
Joni Katajamäki <joni.katajamaki@gmail.com>
2019-01-19 15:14:18 +0200

    replace createAction with Action factory

--- mobx/lib/src/api/action.dart
+++ mobx/lib/src/api/action.dart
@@ -1,43 +1,8 @@
 import 'package:mobx/src/api/context.dart';
 import 'package:mobx/src/core.dart';
 
-/// Creates an action that encapsulates all the mutations happening on the
-/// observables.
-///
-/// Wrapping mutations inside an action ensures the depending observers
-/// are only notified when the action completes. This is useful to silent the notifications
-/// when several observables are being changed together. You will want to run your
-/// reactions only when all the mutations complete. This also helps in keeping
-/// the state of your application consistent.
-///
-/// You can give a debug-friendly [name] to identify the action.
-///
-/// ```
-/// var x = Observable(10);
-/// var y = Observable(20);
-/// var total = Observable(0);
-///
-/// autorun((){
-///   print('x = ${x}, y = ${y}, total = ${total}');
-/// });
-///
-/// var totalUp = action((){
-///   x.value++;
-///   y.value++;
-///
-///   total.value = x.value + y.value;
-/// }, name: 'adder');
-/// ```
-/// Even though we are changing 3 observables (`x`, `y` and `total`), the [autorun()]
-/// is only executed once. This is the benefit of action. It batches up all the change
-/// notifications and propagates them only after the completion of the action. Actions
-/// can also be nested inside, in which case the change notification will propagate when
-/// the top-level action completes.
-Action createAction(Function fn, {String name, ReactiveContext context}) =>
-    Action(context ?? mainContext, fn, name: name);
-
 Action runInAction(Function fn, {String name, ReactiveContext context}) =>
-    Action(context ?? mainContext, fn, name: name)();
+    Action(fn, name: name, context: context)();
 
 /// Untracked ensures there is no tracking derivation while the given action runs.
 /// This is useful in cases where no observers should be linked to a running (tracking) derivation.
--- mobx/lib/src/core/reaction_helper.dart
+++ mobx/lib/src/core/reaction_helper.dart
@@ -74,7 +74,7 @@ ReactionDisposer createReaction<T>(ReactiveContext context,
   final rxnName = name ?? context.nameFor('Reaction');
 
   final effectAction =
-      createAction((T value) => effect(value), name: '$rxnName-effect');
+      Action((T value) => effect(value), name: '$rxnName-effect');
 
   final runSync = delay == null;
   final scheduler = delay != null ? createDelayedScheduler(delay) : null;
@@ -140,7 +140,7 @@ ReactionDisposer createWhenReaction(ReactiveContext context,
     bool Function(Reaction) predicate, void Function() effect,
     {String name, int timeout, void Function(Object, Reaction) onError}) {
   final rxnName = name ?? context.nameFor('When');
-  final effectAction = createAction(effect, name: '$rxnName-effect');
+  final effectAction = Action(effect, name: '$rxnName-effect');
 
   Timer timer;
   ReactionDisposer dispose;
