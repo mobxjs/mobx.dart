f9892c34991d10013cbde791485bc7318e5c80af
pavanpodila <pavan.podila@gmail.com>
2019-01-19 18:31:13 +0530

    moving under /example

--- flutter_mobx/example/test/widget_test.dart
+++ flutter_mobx/example/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:todos/main.dart';
+import 'package:flutter_mobx_example/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
