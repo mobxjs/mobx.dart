bd263f1747580ed75cfec9ff1fae6d82cbd8c288
Ryan <chimon2000@users.noreply.github.com>
2019-02-01 07:20:00 -0500

    [wip] Website docz (#71)
    * Added docz website project
    
    * Added initial docs
    
    * Add netlify config
    
    * Move getting started section ahead of concepts
    
    * netlify settings moved to fix redirects
    
    * Added flutter getting started guide

---
+++ website/doczrc.js
@@ -0,0 +1,20 @@
+export default {
+    title: 'Mobx.dart',
+    description: 'The website for Mobx.dart',
+    src: './docs',
+    debug: false,
+    htmlContext: {
+        head: {}
+    },
+    themeConfig: {
+        logo: {
+            src: 'https://raw.githubusercontent.com/mobxjs/mobx.dart/master/mobx/doc/mobx.png'
+        },
+        mode: 'dark',
+        colors: {
+            primary: '#1389FD'
+        }
+    },
+    menu: ['Home', 'Getting Started', 'Core Concepts', 'Community'],
+    plugins: []
+}
