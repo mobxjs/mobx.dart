dc8b1bb807c7ea098a9483d2e87517b9b57e0418
Joni Katajamäki <joni.katajamaki@gmail.com>
2019-02-02 10:14:05 +0200

    Add replace

--- mobx/lib/src/api/observable_future.dart
+++ mobx/lib/src/api/observable_future.dart
@@ -7,32 +7,62 @@ import 'package:mobx/src/core.dart';
 @experimental
 enum FutureStatus { pending, rejected, fulfilled }
 
+///
 @experimental
 class ObservableFuture<T> implements Future<T> {
+  /// Create a new observable future that tracks the state of the provided future.
   ObservableFuture(Future<T> future, {ReactiveContext context})
-      : this._(context ?? mainContext, future, FutureStatus.pending);
+      : this._(context ?? mainContext, future, FutureStatus.pending, null);
 
-  ObservableFuture._(
-      ReactiveContext context, this._future, FutureStatus initialStatus)
+  /// Create a new future that is completed with a value.
+  ///
+  /// [status] is immediately [FutureStatus.fulfilled].
+  ObservableFuture.value(T value, {ReactiveContext context})
+      : this._(context ?? mainContext, Future.value(value),
+            FutureStatus.fulfilled, value);
+
+  /// Create a new future that is completed with an error.
+  ///
+  /// [status] is immediately [FutureStatus.rejected].
+  ObservableFuture.error(error, {ReactiveContext context})
+      : this._(context ?? mainContext, Future.error(error),
+            FutureStatus.rejected, error);
+
+  ObservableFuture._(ReactiveContext context, this._future,
+      FutureStatus initialStatus, initialResult)
       : _context = context,
         _status = Observable(initialStatus, context: context),
         _actions = ActionController(
             context: context, name: context.nameFor('ObservableFuture<$T>')),
-        _result = Observable(null) {
+        _result = Observable(initialResult) {
     _future.then(_fulfill, onError: _reject);
   }
 
   final ReactiveContext _context;
   final ActionController _actions;
-  final Future<T> _future;
-  final Observable<FutureStatus> _status;
-  final Observable _result;
+  Future<T> _future;
 
+  final Observable<FutureStatus> _status;
+
+  /// Observable status of this.
   FutureStatus get status => _status.value;
 
+  final Observable _result;
+
+  /// Value if this completed with a value.
+  ///
+  /// Null otherwise.
   T get value => status == FutureStatus.fulfilled ? _result.value : null;
 
+  /// Error value if this completed with an error
+  ///
+  /// Null otherwise.
   T get error => status == FutureStatus.rejected ? _result.value : null;
+
+  /// Error or value of this.
+  ///
+  /// Null if this hasn't yet completed.
+  dynamic get result => _result.value;
 
   void _fulfill(T value) {
     final prevDerivation = _actions.startAction();
@@ -54,6 +84,9 @@ class ObservableFuture<T> implements Future<T> {
     }
   }
 
+  /// Maps the current state of this.
+  ///
+  /// Returns null if a handler for the current state is not provided.
   R match<R>(
       {R Function(T) fulfilled,
       // ignore:avoid_annotating_with_dynamic
@@ -69,6 +102,14 @@ class ObservableFuture<T> implements Future<T> {
     return pending == null ? null : pending();
   }
 
+  /// Returns a new future that starts with the [status] and [result] of this.
+  ///
+  /// The [status] and [result] changes when the provided future completes.
+  /// Useful when you don't want to clear the result of the previous operation while
+  /// executing the new operation.
+  ObservableFuture<T> replace(Future<T> nextFuture) =>
+      ObservableFuture<T>._(_context, nextFuture, status, _result.value);
+
   @override
   Stream<T> asStream() => _future.asStream();
 
@@ -76,13 +117,13 @@ class ObservableFuture<T> implements Future<T> {
   ObservableFuture<T> catchError(Function onError,
           {bool Function(Object error) test}) =>
       ObservableFuture._(_context, _future.catchError(onError, test: test),
-          FutureStatus.pending);
+          FutureStatus.pending, null);
 
   @override
   ObservableFuture<R> then<R>(FutureOr<R> Function(T value) onValue,
           {Function onError}) =>
       ObservableFuture._(_context, _future.then(onValue, onError: onError),
-          FutureStatus.pending);
+          FutureStatus.pending, null);
 
   @override
   ObservableFuture<T> timeout(Duration timeLimit,
@@ -90,10 +131,11 @@ class ObservableFuture<T> implements Future<T> {
       ObservableFuture._(
           _context,
           _future.timeout(timeLimit, onTimeout: onTimeout),
-          FutureStatus.pending);
+          FutureStatus.pending,
+          null);
 
   @override
   ObservableFuture<T> whenComplete(FutureOr Function() action) =>
       ObservableFuture._(
-          _context, _future.whenComplete(action), FutureStatus.pending);
+          _context, _future.whenComplete(action), FutureStatus.pending, null);
 }
--- mobx/test/observable_future_test.dart
+++ mobx/test/observable_future_test.dart
@@ -29,14 +29,126 @@ void main() {
       completer.completeError('ERROR');
       try {
         await future;
-      }
-      // ignore:avoid_catches_without_on_clauses
-      catch (error) {
+        // ignore:avoid_catches_without_on_clauses
+      } catch (error) {
         expect(error, equals('ERROR'));
       }
       expect(future.status, equals(FutureStatus.rejected));
       expect(future.value, isNull);
       expect(future.error, equals('ERROR'));
     });
+  });
+
+  test('replace should update value and status after new promise completes',
+      () async {
+    var future = ObservableFuture<String>.error('ERROR');
+    expect(future.status, FutureStatus.rejected);
+    expect(future.result, equals('ERROR'));
+
+    final completer = Completer<String>();
+
+    future = future.replace(completer.future);
+    expect(future.status, FutureStatus.rejected);
+    expect(future.result, equals('ERROR'));
+
+    completer.complete('success');
+    await future;
+
+    expect(future.status, equals(FutureStatus.fulfilled));
+    expect(future.value, equals('success'));
+  });
+
+  test('match works for future completing with a value', () async {
+    final future = ObservableFuture<int>(Future(() => 0));
+
+    String getResult() => future.match(
+          fulfilled: (i) => 'fulfilled',
+          pending: () => 'pending',
+          rejected: (error) => 'rejected',
+        );
+
+    expect(getResult(), equals('pending'));
+    expect(future.status, equals(FutureStatus.pending));
+
+    await future;
+
+    expect(getResult(), equals('fulfilled'));
+    expect(future.status, equals(FutureStatus.fulfilled));
+  });
+
+  test('match works for future completing with an error', () async {
+    final completer = Completer<int>();
+    final future = ObservableFuture<int>(completer.future);
+
+    String getResult() => future.match(
+          fulfilled: (i) => 'fulfilled',
+          pending: () => 'pending',
+          rejected: (error) => 'rejected',
+        );
+
+    expect(getResult(), equals('pending'));
+    expect(future.status, equals(FutureStatus.pending));
+
+    completer.completeError('ERROR');
+    try {
+      await future;
+      // ignore:avoid_catches_without_on_clauses
+    } catch (_) {}
+
+    expect(getResult(), equals('rejected'));
+    expect(future.status, equals(FutureStatus.rejected));
+  });
+
+  test(
+      'match return null if state is pending and no pending matcher is provided',
+      () async {
+    final future = ObservableFuture<int>(Future(() => 0));
+
+    String getResult() => future.match(
+          fulfilled: (i) => 'fulfilled',
+          rejected: (error) => 'rejected',
+        );
+
+    expect(getResult(), equals(null));
+
+    await future;
+    expect(getResult(), equals('fulfilled'));
+  });
+
+  test(
+      'match return null if state is fulfilled and no fulfilled matcher is provided',
+      () async {
+    final future = ObservableFuture<int>(Future(() => 0));
+
+    String getResult() => future.match(
+          pending: () => 'pending',
+          rejected: (error) => 'rejected',
+        );
+
+    expect(getResult(), equals('pending'));
+
+    await future;
+    expect(getResult(), equals(null));
+  });
+
+  test(
+      'match return null if state is rejected and no rejected matcher is provided',
+      () async {
+    final completer = Completer<int>();
+    final future = ObservableFuture<int>(completer.future);
+
+    String getResult() => future.match(
+          pending: () => 'pending',
+          fulfilled: (i) => 'fulfilled',
+        );
+
+    expect(getResult(), equals('pending'));
+
+    completer.completeError('ERROR');
+    try {
+      await future;
+      // ignore:avoid_catches_without_on_clauses
+    } catch (_) {}
+    expect(getResult(), equals(null));
   });
 }
