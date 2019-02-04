33f6eb8acade581100b81391fd8b7ec99e8aff43
Remi Rousselet <darky12s@gmail.com>
2019-02-04 20:54:31 +0100

    add autorun

--- flutter_mobx_hooks/lib/src/use_autorun.dart
+++ flutter_mobx_hooks/lib/src/use_autorun.dart
@@ -1,64 +1,87 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:mobx/mobx.dart';
 
-// TODO: make a class instead, to allow hot-reload on `fn`
+typedef ErrorHandler = void Function(Object, Reaction);
+typedef ReactionHandler = void Function(Reaction);
+
 void useAutorun(
-  Function(Reaction) fn, {
+  ReactionHandler fn, {
   String name,
   int delay,
   ReactiveContext context,
-  void Function(Object, Reaction) onError,
+  ErrorHandler onError,
 }) {
-  useEffect(() {
-    return autorun(
-      fn,
-      name: name,
-      delay: delay,
-      context: context,
-      onError: onError,
-    );
-  }, [fn, name, delay, context, onError]);
+  Hook.use(AutorunHook(
+    fn,
+    name: name,
+    delay: delay,
+    context: context,
+    onError: onError,
+  ));
 }
 
-// TODO: make a class instead, to allow hot-reload on `predicate` and `effect`
-void useWhen(
-  bool Function(Reaction) predicate,
-  void Function() effect, {
-  String name,
-  ReactiveContext context,
-  int timeout,
-  void Function(Object, Reaction) onError,
-}) {
-  useEffect(() {
-    return when(
-      predicate,
-      effect,
-      name: name,
-      timeout: timeout,
-      context: context,
-      onError: onError,
-    );
-  }, [predicate, effect, name, timeout, context, onError]);
+class AutorunHook extends Hook<void> {
+  const AutorunHook(
+    this.fn, {
+    this.onError,
+    this.delay,
+    this.name,
+    this.context,
+  });
+
+  static ReactionDisposer Function(
+    Function(Reaction) fn, {
+    String name,
+    int delay,
+    ReactiveContext context,
+    void Function(Object, Reaction) onError,
+  }) run = autorun;
+
+  final ReactionHandler fn;
+  final ErrorHandler onError;
+  final int delay;
+  final String name;
+  final ReactiveContext context;
+
+  @override
+  _AutorunHookState createState() => _AutorunHookState();
 }
 
-// TODO: make a class instead, to allow hot-reload on `predicate` and `effect`
-void useReaction<T>(
-  T Function(Reaction) predicate,
-  void Function(T) effect, {
-  String name,
-  int delay,
-  bool fireImmediately,
-  ReactiveContext context,
-  void Function(Object, Reaction) onError,
-}) {
-  useEffect(() {
-    return reaction(
-      predicate,
-      effect,
-      name: name,
-      fireImmediately: fireImmediately,
-      context: context,
-      onError: onError,
+class _AutorunHookState extends HookState<void, AutorunHook> {
+  ReactionDisposer disposer;
+
+  ReactiveContext get _reactiveContext => hook.context ?? mainContext;
+
+  @override
+  void initHook() {
+    _run();
+  }
+
+  @override
+  void didUpdateHook(AutorunHook oldHook) {
+    if (hook.context != oldHook.context) {
+      disposer();
+      _run();
+    }
+    // TODO: hot reload name/delay/onError
+  }
+
+  void _run() {
+    disposer = AutorunHook.run(
+      hook.fn,
+      onError: hook.onError,
+      context: _reactiveContext,
+      delay: hook.delay,
+      name: hook.name,
     );
-  }, [predicate, effect, name, fireImmediately, context, onError]);
+  }
+
+  @override
+  void build(BuildContext context) {}
+
+  @override
+  void dispose() {
+    disposer();
+  }
 }
