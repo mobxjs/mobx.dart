27dbc5057489c85992aec384e16c152e6c687504
pavanpodila <pavan.podila@gmail.com>
2019-02-06 15:17:04 +0530

    fixing lint

--- mobx/test/shared_mocks.dart
+++ mobx/test/shared_mocks.dart
@@ -1,9 +1,6 @@
 import 'package:mobx/mobx.dart';
 import 'package:mockito/mockito.dart';
 
-class MockContext extends Mock implements ReactiveContext {
-  @override
-  int computationDepth = 0;
-}
+class MockContext extends Mock implements ReactiveContext {}
 
 class MockAtom extends Mock implements Atom {}
