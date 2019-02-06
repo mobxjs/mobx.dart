061bdf31799d6dd988dcb824afcc472970c39fe9
Pavan Podila <pavanpodila@users.noreply.github.com>
2019-02-06 17:04:53 +0530

    Enforce actions (#88)
    * adding a check for computed that mutate observables inside
    * wrapping actions with allowStateChanges
    * fixing lint
    * passing tests
    * making a constructor for ActionRunInfo

--- mobx/test/action_controller_test.dart
+++ mobx/test/action_controller_test.dart
@@ -1,6 +1,6 @@
 import 'package:mobx/src/core.dart';
-import 'package:test/test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
 
 import 'shared_mocks.dart';
 
@@ -8,29 +8,36 @@ class MockDerivation extends Mock implements Derivation {}
 
 void main() {
   group('ActionController', () {
-    test('startAction calls context.untrackedStart and startBatch', () {
+    test(
+        'startAction calls startUntracked, startBatch and startAllowStateChanges',
+        () {
       final context = MockContext();
       ActionController(context: context).startAction();
 
       verifyInOrder([
-        context.untrackedStart(),
+        context.startUntracked(),
         context.startBatch(),
+        context.startAllowStateChanges(allow: true),
       ]);
     });
 
-    test('endAction calls context.endBatch and untrackedEnd', () {
+    test('endAction calls endAllowStateChanges, endBatch and endUntracked', () {
       final context = MockContext();
       final prevDerivation = MockDerivation();
 
-      when(context.untrackedStart()).thenReturn(prevDerivation);
+      when(context.startUntracked()).thenReturn(prevDerivation);
+
+      final runInfo = ActionRunInfo(
+          prevDerivation: prevDerivation, prevAllowStateChanges: false);
 
       ActionController(context: context)
         ..startAction()
-        ..endAction(prevDerivation);
+        ..endAction(runInfo);
 
       verifyInOrder([
+        context.endAllowStateChanges(allow: false),
         context.endBatch(),
-        context.untrackedEnd(prevDerivation),
+        context.endUntracked(prevDerivation),
       ]);
     });
   });
--- mobx/test/context_test.dart
+++ mobx/test/context_test.dart
@@ -23,5 +23,29 @@ void main() {
       dispose();
       dispose1();
     });
+
+    test('can change observables inside computed if there are no observers',
+        () {
+      final x = Observable(0);
+
+      final c = Computed(() => x.value++);
+
+      expect(() => c.value, returnsNormally);
+    });
+
+    test('cannot change observables inside computed if they have observers',
+        () {
+      final x = Observable(0);
+
+      final c = Computed<int>(() => x.value++);
+
+      expect(() {
+        final d = autorun((_) => x.value);
+        // Fetch the value which is in turn mutating the observable (x.value).
+        // This is not allowed because there is an observer: autorun.
+        c.value;
+        d();
+      }, throwsException);
+    });
   });
 }
