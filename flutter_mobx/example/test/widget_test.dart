145060b0ffcc0ae02f18250facbb422d0eb081c7
pavanpodila <pavan.podila@gmail.com>
2019-02-07 19:59:44 +0530

    fixing lint

--- flutter_mobx/example/test/widget_test.dart
+++ flutter_mobx/example/test/widget_test.dart
@@ -1,6 +1,5 @@
+import 'package:flutter_mobx_example/main.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import '../lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
