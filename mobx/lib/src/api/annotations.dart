4e9709ce23943a4a403be872feab16952e5de842
Marco Scannadinari <m@scannadinari.co.uk>
2019-02-07 13:54:42 +0000

    Fix typo

--- mobx/lib/src/api/annotations.dart
+++ mobx/lib/src/api/annotations.dart
@@ -18,7 +18,7 @@ class MakeAction {
   const MakeAction._();
 }
 
-/// Declared a method as an action. See the `Action` class for full
+/// Declares a method as an action. See the `Action` class for full
 /// documentation.
 const MakeAction action = MakeAction._();
 
