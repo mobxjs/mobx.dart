9e9ab7cddbf8955db71f5f7a4b61181736531ba7
pavanpodila <pavan.podila@gmail.com>
2019-02-10 12:03:52 +0530

    version update for mobx_codegen

--- flutter_mobx/example/lib/todos/todo_list.g.dart
+++ flutter_mobx/example/lib/todos/todo_list.g.dart
@@ -6,38 +6,6 @@ part of 'todo_list.dart';
 // StoreGenerator
 // **************************************************************************
 
-mixin _$Todo on TodoBase, Store {
-  final _$descriptionAtom = Atom(name: 'TodoBase.description');
-
-  @override
-  String get description {
-    _$descriptionAtom.reportObserved();
-    return super.description;
-  }
-
-  @override
-  set description(String value) {
-    mainContext.checkIfStateModificationsAreAllowed(_$descriptionAtom);
-    super.description = value;
-    _$descriptionAtom.reportChanged();
-  }
-
-  final _$doneAtom = Atom(name: 'TodoBase.done');
-
-  @override
-  bool get done {
-    _$doneAtom.reportObserved();
-    return super.done;
-  }
-
-  @override
-  set done(bool value) {
-    mainContext.checkIfStateModificationsAreAllowed(_$doneAtom);
-    super.done = value;
-    _$doneAtom.reportChanged();
-  }
-}
-
 mixin _$TodoList on TodoListBase, Store {
   Computed<ObservableList<Todo>> _$pendingTodosComputed;
 
