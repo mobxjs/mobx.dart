21e3aa33d8de412d5c223cbdd13bca31b36e4403
pavanpodila <pavan.podila@gmail.com>
2019-02-14 08:22:17 +0530

    checking some commands

---
+++ tool/circle_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+#export TOOL_DIR="$PWD/tool"
+#export FLUTTER_CMD="$PWD/flutter/bin/flutter"
+
+cd $PACKAGE
+$TOOL_DIR/get.sh
+$TOOL_DIR/analyze.sh
+$TOOL_DIR/test.sh
+
+bash <(curl -s https://codecov.io/bash) -c -F $PACKAGE
