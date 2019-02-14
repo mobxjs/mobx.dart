9cfebfefc51b466046c9671e4fff8c7be3661f4d
pavanpodila <pavan.podila@gmail.com>
2019-02-14 13:55:04 +0530

    testing circleci

--- tool/circle_run.sh
+++ tool/circle_run.sh
@@ -5,7 +5,6 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-cd mobx
 pub get
 dartanalyzer --fatal-infos --fatal-warnings .
-dart --enable_asserts --enable-vm-service test/all.dart
+pub run test_coverage
