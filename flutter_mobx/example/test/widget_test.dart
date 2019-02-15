b310e42237ae7d2d235e62df5ea8a14b4a88b0eb
pavanpodila <pavan.podila@gmail.com>
2019-02-15 10:19:47 +0530

    sdk update

--- flutter_mobx/example/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_mobx_example/main.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
-  });
-}
