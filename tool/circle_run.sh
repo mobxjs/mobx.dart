addf762bb9bb47060a683101391c8e4346b21dfd
pavanpodila <pavan.podila@gmail.com>
2019-02-15 07:15:03 +0530

    switching to git url for flutter

--- tool/circle_run.sh
+++ tool/circle_run.sh
@@ -10,10 +10,10 @@ if [[ "${FLUTTER}" = true ]]
 then
   git clone https://github.com/flutter/flutter.git -b stable --depth 1
   FLUTTER_CMD=./flutter/bin/flutter
-  FLUTTER_CMD doctor
-  FLUTTER_CMD packages get
-  FLUTTER_CMD analyze
-  FLUTTER_CMD test --coverage --coverage-path coverage/lcov.info
+  $FLUTTER_CMD doctor
+  $FLUTTER_CMD packages get
+  $FLUTTER_CMD analyze
+  $FLUTTER_CMD test --coverage --coverage-path coverage/lcov.info
 else
   pub get
   dartanalyzer --fatal-infos --fatal-warnings .
