58461e731195e8d7605996851dc81d63599dee96
Joni Katajamäki <joni.katajamaki@gmail.com>
2019-02-24 19:41:56 +0200

    Remove unused import

--- mobx_codegen/lib/src/mobx_codegen_base.dart
+++ mobx_codegen/lib/src/mobx_codegen_base.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/dart/element/type.dart';
 import 'package:analyzer/dart/element/visitor.dart';
 import 'package:build/build.dart';
 import 'package:build/src/builder/build_step.dart';
