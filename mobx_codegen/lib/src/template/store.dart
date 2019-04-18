29590b17877976826d59ee9cf787a7d9a4213b30
Marco Scannadinari <4211302+marcoms@users.noreply.github.com>
2019-04-18 21:11:41 +0100

    Ignore lines 80+ characters long in generated code

--- mobx_codegen/lib/src/template/store.dart
+++ mobx_codegen/lib/src/template/store.dart
@@ -33,7 +33,7 @@ class StoreTemplate {
 
   @override
   String toString() => """
-  // ignore_for_file: non_constant_identifier_names, unnecessary_lambdas, prefer_expression_function_bodies
+  // ignore_for_file: non_constant_identifier_names, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars
 
   mixin $mixinName$typeParams on $parentName$typeArgs, Store {
     $computeds
