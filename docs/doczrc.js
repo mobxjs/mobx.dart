7bbe6790b215f1ffead1c1cde753662d42f2d6d4
pavanpodila <pavan@pixelingene.com>
2019-07-14 14:54:07 +0530

    adding favicon

--- docs/doczrc.js
+++ docs/doczrc.js
@@ -6,6 +6,9 @@ export default {
     files: "**/*.{md,markdown,mdx}",
     repository: "https://github.com/mobxjs/mobx.dart",
     indexHtml: "src/index.html",
+    htmlContext: {
+        favicon: 'https://raw.githubusercontent.com/mobxjs/mobx.dart/master/docs/src/images/mobx.png'
+    },
     themeConfig: {
         logo: {
             src:
