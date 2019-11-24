4d1184b7d56d433151dc78e50c50a769b22e72ed
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2019-11-24 13:09:56 +1100

    add connectivity example that demonstrates using a reaction (#319)
    * add connectivity example and update version of mobx used for reaction to run
    
    * fix grammar in comment on using a delay with reaction

---
+++ mobx_examples/lib/connectivity/connectivity_store.dart
@@ -0,0 +1,15 @@
+import 'package:mobx/mobx.dart';
+import 'package:connectivity/connectivity.dart';
+
+part 'connectivity_store.g.dart';
+
+class ConnectivityStore = _ConnectivityStore with _$ConnectivityStore;
+
+abstract class _ConnectivityStore with Store {
+  _ConnectivityStore() {
+    connectivityStream = ObservableStream(Connectivity().onConnectivityChanged);
+  }
+
+  @observable
+  ObservableStream<ConnectivityResult> connectivityStream;
+}
---
+++ mobx_examples/lib/connectivity/connectivity_store.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'connectivity_store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$ConnectivityStore on _ConnectivityStore, Store {
+  final _$connectivityStreamAtom =
+      Atom(name: '_ConnectivityStore.connectivityStream');
+
+  @override
+  ObservableStream<ConnectivityResult> get connectivityStream {
+    _$connectivityStreamAtom.context
+        .enforceReadPolicy(_$connectivityStreamAtom);
+    _$connectivityStreamAtom.reportObserved();
+    return super.connectivityStream;
+  }
+
+  @override
+  set connectivityStream(ObservableStream<ConnectivityResult> value) {
+    _$connectivityStreamAtom.context.conditionallyRunInAction(() {
+      super.connectivityStream = value;
+      _$connectivityStreamAtom.reportChanged();
+    }, _$connectivityStreamAtom, name: '${_$connectivityStreamAtom.name}_set');
+  }
+}
