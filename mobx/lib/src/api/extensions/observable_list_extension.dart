b200b912c87def0956984e66ce144b43eb90061e
Jacob Moura <jacobaraujo7@gmail.com>
2019-12-27 11:55:08 -0300

    added RectiveContext in asObservable

--- mobx/lib/src/api/extensions/observable_list_extension.dart
+++ mobx/lib/src/api/extensions/observable_list_extension.dart
@@ -2,5 +2,6 @@ part of '../extensions.dart';
 
 /// Turn the List into an ObservableList.
 extension ObservableListExtension<T> on List<T> {
-  ObservableList<T> asObservable() => ObservableList<T>.of(this);
+  ObservableList<T> asObservable({ReactiveContext context}) =>
+      ObservableList<T>.of(this, context: context);
 }
--- mobx/lib/src/api/extensions/observable_set_extension.dart
+++ mobx/lib/src/api/extensions/observable_set_extension.dart
@@ -2,5 +2,6 @@ part of '../extensions.dart';
 
 /// Turn the Set into an ObservableSet.
 extension ObservableSetExtension<T> on Set<T> {
-  ObservableSet<T> asObservable() => ObservableSet<T>.of(this);
+  ObservableSet<T> asObservable({ReactiveContext context}) =>
+      ObservableSet<T>.of(this, context: context);
 }
