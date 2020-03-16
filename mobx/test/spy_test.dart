a81031e00242de478146373a68455af27c3f970d
Pavan Podila <pavan.podila@gmail.com>
2020-03-16 08:56:46 +0530

    more updates to the spec and starting with a test

--- mobx/test/spy_test.dart
+++ mobx/test/spy_test.dart
@@ -1,13 +1,8 @@
 import 'package:test/test.dart';
-import 'package:mobx/mobx.dart';
 
 void main() {
   group('Spy', () {
     test('spies can be added', () {});
     test('when a spy unsubscribes, it is removed from the list', () {});
-    test('spies are enabled', () {
-      final context = ReactiveContext();
-      expect(context.isSpyEnabled, isTrue);
-    });
   });
 }
