90b6b70daa469d20c35bd65829777f00c987c82b
Pavan Podila <pavan.podila@gmail.com>
2020-03-16 08:56:46 +0530

    more updates to the spec and starting with a test

---
+++ mobx/test/spy_test.dart
@@ -0,0 +1,8 @@
+import 'package:test/test.dart';
+
+void main() {
+  group('Spy', () {
+    test('spies can be added', () {});
+    test('when a spy unsubscribes, it is removed from the list', () {});
+  });
+}
