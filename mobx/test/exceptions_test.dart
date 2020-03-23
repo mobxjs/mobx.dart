fd0f36f0e7bf3caade57d7614cd34d77e292258f
Pavan Podila <pavan.podila@gmail.com>
2020-03-23 23:49:12 +0530

    improving test coverage

--- mobx/test/exceptions_test.dart
+++ mobx/test/exceptions_test.dart
@@ -2,15 +2,24 @@ import 'package:mobx/mobx.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('MobXException has the right toString()', (){
+  test('MobXException has the right toString()', () {
     final ex = MobXException('Test Exception');
 
     expect(ex.toString(), equals('Test Exception'));
   });
 
-  test('MobXCyclicReactionException has the right toString()', (){
+  test('MobXCyclicReactionException has the right toString()', () {
     final ex = MobXCyclicReactionException('Test Exception');
 
     expect(ex.toString(), equals('Test Exception'));
+  });
+
+  test('MobXCaughtException contains the stacktrace', () {
+    try {
+      throw Exception('test');
+    } on Object catch (e, s) {
+      final ex = MobXCaughtException(e, stackTrace: s);
+      expect(ex.stackTrace, isNotNull);
+    }
   });
 }
