1ef5a958adf1dc21e3a5f81ce7c83fbb8a266deb
Pavan Podila <pavan.podila@gmail.com>
2020-04-03 11:08:47 +0530

    fixing tests in mobx

--- mobx/test/shared_mocks.dart
+++ mobx/test/shared_mocks.dart
@@ -1,7 +1,10 @@
 import 'package:mobx/mobx.dart';
 import 'package:mockito/mockito.dart';
 
-class MockContext extends Mock implements ReactiveContext {}
+class MockContext extends Mock implements ReactiveContext {
+  @override
+  bool get isSpyEnabled => true;
+}
 
 class MockAtom extends Mock implements Atom {
   MockAtom() {
