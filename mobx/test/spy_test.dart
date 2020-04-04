b5b9af88952a1d8fdc8139881d6e82c178122a71
Pavan Podila <pavan.podila@gmail.com>
2020-04-04 14:34:09 +0530

    improving coverage and adding missing tests

--- mobx/test/spy_test.dart
+++ mobx/test/spy_test.dart
@@ -24,17 +24,49 @@ void main() {
     test('spy-event is raised when an Observable is created', () {
       final d = mainContext.spy((event) {
         expect(event, isA<ObservableValueSpyEvent>());
-        expect((event as ObservableValueSpyEvent).newValue, equals(0));
+
+        final obj = event as ObservableValueSpyEvent;
+        expect(obj.newValue, equals(0));
+        expect(obj.toString(), equals('observable test=0, previously=null'));
       });
 
-      Observable(0);
+      Observable(0, name: 'test');
+
+      d();
+    });
+
+    test('spy-event is raised when an Observable is updated', () {
+      final o = Observable(0, name: 'test');
+
+      var eventRaised = false;
+      var endEventRaised = false;
+
+      final d = mainContext.spy((event) {
+        if (event is ObservableValueSpyEvent) {
+          eventRaised = true;
+
+          expect(event.toString(),
+              equals('observable(START) test=1, previously=0'));
+        }
+
+        if (event is EndedSpyEvent && event.type == 'observable') {
+          endEventRaised = true;
+
+          expect(event.toString(), equals('observable(END) test'));
+        }
+      });
+
+      o.value++;
+
+      expect(eventRaised, isTrue);
+      expect(endEventRaised, isTrue);
 
       d();
     });
 
     test('spy-event is raised when a Computed is evaluated', () {
       final x = Observable(0);
-      final y = Computed(() => x.value + 1);
+      final y = Computed(() => x.value + 1, name: 'test');
 
       // Create a reaction so as to force the computed value to be read
       // ComputedSpyEvent is not raised outside of a reaction (aka batch)
@@ -44,6 +76,8 @@ void main() {
       final d = mainContext.spy((event) {
         if (event is ComputedValueSpyEvent) {
           wasComputedSpyEventRaised = true;
+
+          expect(event.toString(), equals('computed test'));
         }
       });
 
@@ -57,33 +91,56 @@ void main() {
 
     test('spy-event is raised when an Action is executed', () {
       var eventRaised = false;
+      var endEventRaised = false;
       final d = mainContext.spy((event) {
         if (event is ActionSpyEvent) {
           eventRaised = true;
+
+          expect(event.toString(), equals('action(START) test'));
+        }
+
+        if (event is EndedSpyEvent && event.type == 'action') {
+          endEventRaised = true;
+
+          expect(
+              event.toString(), matches(RegExp(r'action\(END [^\)]*\) test')));
         }
       });
 
-      runInAction(() {});
+      runInAction(() {}, name: 'test');
 
       expect(eventRaised, isTrue);
+      expect(endEventRaised, isTrue);
 
       d();
     });
 
     test('spy-event is raised when a Reaction is executed', () {
       final o = Observable(0);
-      final d1 = reaction((_) => o.value, (_) {});
+      final d1 = reaction((_) => o.value, (_) {}, name: 'test');
 
       var eventRaised = false;
+      var endEventRaised = false;
+
       final d = mainContext.spy((event) {
         if (event is ReactionSpyEvent) {
           eventRaised = true;
+
+          expect(event.toString(), equals('reaction(START) test'));
+        }
+
+        if (event is EndedSpyEvent && event.type == 'reaction') {
+          endEventRaised = true;
+
+          expect(event.toString(),
+              matches(RegExp(r'reaction\(END [^\)]*\) test')));
         }
       });
 
       o.value++; // force the reaction to run
 
       expect(eventRaised, isTrue);
+      expect(endEventRaised, isTrue);
 
       d();
       d1();
@@ -95,12 +152,14 @@ void main() {
         if (o.value == 1) {
           throw Exception('test failure');
         }
-      }, (_) {});
+      }, (_) {}, name: 'test');
 
       var eventRaised = false;
       final d = mainContext.spy((event) {
         if (event is ReactionErrorSpyEvent) {
           eventRaised = true;
+
+          expect(event.toString(), startsWith('reaction-error test'));
         }
       });
 
@@ -114,18 +173,42 @@ void main() {
 
     test('spy-event is raised when a Reaction disposes', () {
       final o = Observable(0);
-      final d1 = reaction((_) => o.value, (_) {});
+      final d1 = reaction((_) => o.value, (_) {}, name: 'test');
 
       var eventRaised = false;
       final d = mainContext.spy((event) {
         if (event is ReactionDisposedSpyEvent) {
           eventRaised = true;
+
+          expect(event.toString(), equals('reaction-dispose test'));
         }
       });
 
       o.value++; // force the reaction to run
 
       d1();
+      expect(eventRaised, isTrue);
+
+      d();
+    });
+
+    test('calling atom read/write raises a spy event', () {
+      final atom = Atom(name: 'test');
+      var eventRaised = false;
+
+      final d = mainContext.spy((event) {
+        if (event is EndedSpyEvent && event.type == 'observable') {
+          eventRaised = true;
+
+          expect(event.toString(), equals('observable(END) test'));
+        }
+      });
+
+      atom.reportRead();
+
+      // ignore: cascade_invocations
+      atom.reportWrite('test', null, () {});
+
       expect(eventRaised, isTrue);
 
       d();
