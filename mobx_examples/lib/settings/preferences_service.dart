9cb02c177ae598c0e3aba72948d3831aa9e3e031
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2020-04-05 04:16:38 +1000

    bump dependencies used by mobx_examples, fix settings example and add reflectable code (#462)
    * add generated code from reflectable package
    
    * bump dependencies used by example app
    
    * delete widget test from mobx_examples
    
    * fix issue with settings example on restoring preferencing on using dark mode

--- mobx_examples/lib/settings/preferences_service.dart
+++ mobx_examples/lib/settings/preferences_service.dart
@@ -1,22 +1,14 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class PreferencesService {
-  PreferencesService() {
-    loaded = _loadPreferences();
-  }
-
-  Future<void> loaded;
+  PreferencesService(this._sharedPreferences);
 
   static const String _useDarkModeKey = 'useDarkMode';
-  SharedPreferences _sharedPreferences;
+  final SharedPreferences _sharedPreferences;
 
   set useDarkMode(bool useDarkMode) {
     _sharedPreferences.setBool(_useDarkModeKey, useDarkMode);
   }
 
   bool get useDarkMode => _sharedPreferences.getBool(_useDarkModeKey) ?? false;
-
-  Future<void> _loadPreferences() async {
-    _sharedPreferences = await SharedPreferences.getInstance();
-  }
 }
--- mobx_examples/lib/settings/settings_store.dart
+++ mobx_examples/lib/settings/settings_store.dart
@@ -8,7 +8,7 @@ class SettingsStore = _SettingsStore with _$SettingsStore;
 
 abstract class _SettingsStore with Store {
   _SettingsStore(this._preferencesService) {
-    _setup();
+    useDarkMode = _preferencesService.useDarkMode;
   }
 
   final PreferencesService _preferencesService;
@@ -18,13 +18,7 @@ abstract class _SettingsStore with Store {
 
   @action
   Future<void> setDarkMode({@required bool value}) async {
-    await _preferencesService.loaded;
     _preferencesService.useDarkMode = value;
     useDarkMode = value;
-  }
-
-  Future<void> _setup() async {
-    await _preferencesService.loaded;
-    useDarkMode = _preferencesService.useDarkMode;
   }
 }
