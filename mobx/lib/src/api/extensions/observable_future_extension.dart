6f9b9332e4582e224bd500fab4d02ae46196ae21
LucazzP <lucashpolazzo2013@gmail.com>
2020-04-07 18:02:48 -0300

    [feat] Added map function on Future extension

--- mobx/lib/src/api/extensions/observable_future_extension.dart
+++ mobx/lib/src/api/extensions/observable_future_extension.dart
@@ -4,4 +4,7 @@ part of '../extensions.dart';
 extension ObservableFutureExtension<T> on Future<T> {
   ObservableFuture<T> asObservable({ReactiveContext context, String name}) =>
       ObservableFuture<T>(this, context: context, name: name);
+  /// Transform the future value into other type after complete future
+  /// Useful to transform list to ObservableList in requests
+  Future<E> map<E>(Future<E> Function(Future<T>) transform) => transform(this);
 }
