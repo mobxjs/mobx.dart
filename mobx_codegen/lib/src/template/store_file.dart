fa8bdfedb6691b501d51acba2817bf31190bf6a0
Ivan Terekhin <i.terhin@gmail.com>
2020-06-09 18:05:21 +0300

    Template addition

--- mobx_codegen/lib/src/template/store_file.dart
+++ mobx_codegen/lib/src/template/store_file.dart
@@ -1,5 +1,5 @@
 const _analyzerIgnores =
-    '// ignore_for_file: non_constant_identifier_names, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic';
+    '// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic';
 
 /// Template for a file containing one or more generated stores.
 class StoreFileTemplate {
