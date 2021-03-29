f4cef0e7ccbedda3f65ec492a9217c86f2401954
Ascênio <Ascenio@users.noreply.github.com>
2021-03-29 00:17:34 -0300

    Adds @readonly annotation (#647)

--- mobx/lib/mobx.dart
+++ mobx/lib/mobx.dart
@@ -30,7 +30,7 @@ library mobx;
 
 export 'package:mobx/src/api/action.dart';
 export 'package:mobx/src/api/annotations.dart'
-    show action, computed, observable, StoreConfig;
+    show action, computed, readonly, observable, StoreConfig;
 export 'package:mobx/src/api/async.dart'
     show
         ObservableFuture,
