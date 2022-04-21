9208c8aea7d6a61431130ba2b3f9c1cbedb7dfeb
Sergey Molchanovsky <subzero911@yandex.ru>
2022-04-21 11:20:02 +0200

    Observable extensions (#792)
    * primitive type aliases;
    asObservable() ext. for primitive types
    
    * toggle
    
    * naming fixes
    
    * tests
    
    * bump ver. 2.0.7
    
    * mobx.dart version increment
    
    * changelog for 2.0.7
    
    * API docs
    
    * requested changed to changelog
    
    Co-authored-by: Pavan Podila <pavanpodila@users.noreply.github.com>

--- mobx/lib/src/api/extensions.dart
+++ mobx/lib/src/api/extensions.dart
@@ -1,5 +1,6 @@
 import '../core.dart';
 import 'async.dart';
+import 'action.dart';
 import 'observable_collections.dart';
 
 part 'extensions/observable_stream_extension.dart';
@@ -7,3 +8,4 @@ part 'extensions/observable_future_extension.dart';
 part 'extensions/observable_list_extension.dart';
 part 'extensions/observable_set_extension.dart';
 part 'extensions/observable_map_extension.dart';
+part 'extensions/primitive_types_extensions.dart';
