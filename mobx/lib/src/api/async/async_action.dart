251f1dc7e5874474cdafcd3bc1e0b74d96eeb26e
Vursin <contact@vursin.com>
2022-07-05 01:28:01 +0700

    Add `newBehavior` variable to the `AsyncAction`

--- mobx/lib/src/api/async/async_action.dart
+++ mobx/lib/src/api/async/async_action.dart
@@ -6,13 +6,16 @@ part of '../async.dart';
 /// You would rarely need to use this class directly. Instead, use the `@action` annotation along with
 /// the `mobx_codegen` package.
 class AsyncAction {
-  AsyncAction(String name, {ReactiveContext? context})
-      : this._(context ?? mainContext, name);
+  AsyncAction(String name, {ReactiveContext? context, bool newBehavior = false})
+      : this._(context ?? mainContext, name, newBehavior);
 
-  AsyncAction._(ReactiveContext context, String name)
+  AsyncAction._(ReactiveContext context, String name, this.newBehavior)
       : _actions = ActionController(context: context, name: name);
 
   final ActionController _actions;
+
+  // TODO: Remove this when new feature `asyncAction` is available
+  final bool newBehavior;
 
   Zone? _zoneField;
   Zone get _zone {
@@ -24,6 +27,10 @@ class AsyncAction {
   }
 
   Future<R> run<R>(Future<R> Function() body) async {
+    if (!newBehavior) {
+      return _runOld(body);
+    }
+
     final actionInfo = _actions.startAction(name: _actions.name);
     try {
       return await _zone.run(body);
@@ -40,6 +47,10 @@ class AsyncAction {
   static dynamic _noOp() => null;
 
   R _run<R>(Zone self, ZoneDelegate parent, Zone zone, R Function() f) {
+    if (!newBehavior) {
+      return __runOld(self, parent, zone, f);
+    }
+
     final result = parent.run(zone, f);
     return result;
   }
@@ -48,8 +59,53 @@ class AsyncAction {
   // when a result is produced
   R _runUnary<R, A>(
       Zone self, ZoneDelegate parent, Zone zone, R Function(A a) f, A a) {
+    if (!newBehavior) {
+      return __runUnaryOld(self, parent, zone, f, a);
+    }
+
     final result = parent.runUnary(zone, f, a);
     return result;
+  }
+
+  // TODO: Remove this when new feature `asyncAction` is available
+  Future<R> _runOld<R>(Future<R> Function() body) async {
+    // final actionInfo = _actions.startAction(name: _actions.name);
+    try {
+      return await _zone.run(body);
+    } finally {
+      // @katis:
+      // Delay completion until next microtask completion.
+      // Needed to make sure that all mobx state changes are
+      // applied after `await run()` completes, not sure why.
+      await Future.microtask(_noOp);
+      // _actions.endAction(actionInfo);
+    }
+  }
+
+  // TODO: Remove this when new feature `asyncAction` is available
+  R __runOld<R>(Zone self, ZoneDelegate parent, Zone zone, R Function() f) {
+    final actionInfo = _actions.startAction(name: '${_actions.name}(Zone.run)');
+    try {
+      final result = parent.run(zone, f);
+      return result;
+    } finally {
+      _actions.endAction(actionInfo);
+    }
+  }
+
+  // Will be invoked for a catch clause that has a single argument: exception or
+  // when a result is produced
+  // TODO: Remove this when new feature `asyncAction` is available
+  R __runUnaryOld<R, A>(
+      Zone self, ZoneDelegate parent, Zone zone, R Function(A a) f, A a) {
+    final actionInfo =
+        _actions.startAction(name: '${_actions.name}(Zone.runUnary)');
+    try {
+      final result = parent.runUnary(zone, f, a);
+      return result;
+    } finally {
+      _actions.endAction(actionInfo);
+    }
   }
 
   // Will be invoked for a catch clause that has two arguments: exception and stacktrace
