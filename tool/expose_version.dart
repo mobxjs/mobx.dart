cea7013fad1fcdfbb3213045db9be5e3f311f2f3
pavanpodila <pavan@pixelingene.com>
2022-09-19 08:32:48 +0530

    simplifying set_version a bit more

--- tool/expose_version.dart
+++ tool/expose_version.dart
@@ -3,20 +3,21 @@
 import 'dart:io';
 
 void main() {
-  final pubspecFile = File('${Directory.current.path}/pubspec.yaml');
+  final packagePath = Directory.current.path;
+  final packageName = Directory.current.path.split("/").last;
+  final pubspecFile = File('$packagePath/pubspec.yaml');
 
   final projectData = pubspecFile.readAsStringSync();
   final versionNumber = resolveVersion(projectData);
 
   if (versionNumber == null) {
-    final package = Directory.current.path.split("/").last;
-    print('Failed to fetch version for $package');
+    print('Failed to fetch version for $packageName');
     exit(1);
   }
 
   final versionText = generateVersionScript(versionNumber);
 
-  final versionFile = File('${Directory.current.path}/lib/version.dart');
+  final versionFile = File('$packagePath/lib/version.dart');
   versionFile.writeAsStringSync(versionText);
 }
 
