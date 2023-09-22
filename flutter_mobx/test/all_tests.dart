42a941368751d0509b8b5731f186140585457c58
Minsu Lee <amond@amond.net>
2023-09-22 18:45:16 +0900

    feat: add `MultiReactionBuilder` widget

--- flutter_mobx/test/all_tests.dart
+++ flutter_mobx/test/all_tests.dart
@@ -1,7 +1,9 @@
 import 'flutter_mobx_test.dart' as flutter_mobx_test;
 import 'reaction_builder_test.dart' as reaction_builder_test;
+import 'multi_reaction_builder_test.dart' as multi_reaction_builder_test;
 
 void main() {
   flutter_mobx_test.main();
   reaction_builder_test.main();
+  multi_reaction_builder_test.main();
 }
