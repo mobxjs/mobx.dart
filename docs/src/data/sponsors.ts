0768592a86589be32fc73695f44e2497ee142bbe
pavanpodila <pavan@vyuh.tech>
2024-02-29 17:50:15 +0530

    updating readme

--- docs/src/data/sponsors.ts
+++ docs/src/data/sponsors.ts
@@ -1,13 +1,8 @@
 export const sponsors = [
   {
-    logo: require('../images/publicis-sapient-sponsor.png').default,
-    url: 'https://publicis.sapient.com',
-    active: false,
-  },
-  {
-    logo: require('../images/wunderdog-sponsor.png').default,
-    url: 'https://wunderdog.fi',
-    active: false,
+    logo: require('../images/vyuh-sponsor.png').default,
+    url: 'https://vyuh.tech',
+    active: true,
   },
   {
     logo: 'https://www.netlify.com/img/global/badges/netlify-color-bg.svg',
@@ -18,5 +13,15 @@ export const sponsors = [
     logo: require('../images/algolia-sponsor.png').default,
     url: 'https://www.algolia.com',
     active: true,
+  },
+  {
+    logo: require('../images/wunderdog-sponsor.png').default,
+    url: 'https://wunderdog.fi',
+    active: false,
+  },
+  {
+    logo: require('../images/publicis-sapient-sponsor.png').default,
+    url: 'https://publicis.sapient.com',
+    active: false,
   },
 ];
